24dfe907f2f3255b05b290d97285807d0669abbb
Nicolas Reymond <n.reymond.12@gmail.com>
2020-06-26 10:23:46 +0200

    [WIP] Improve component

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,55 +1,23 @@
 <template>
-  <ul>
-    <input type="button" value="Prev" />
-    <input type="button" value="Next" />
-    <p>
-      <strong>{{posts[0].title}}</strong>
-    </p>
-    <img :src="posts[0].hdurl" alt="APOD" />
-    <strong>{{posts[0].copyright}}</strong>
-    <!-- <p>{{post.visual_and_thumbnail_description}}</p> -->
-  </ul>
-
-  <!-- <ul v-if="errors && errors.length">
-    <li v-for="error of errors">{{error.message}}</li>
-  </ul>-->
+  <div id="app">
+    {{ msg }}
+    <SimpleComponent />
+  </div>
 </template>
 
 <script>
-import axios from "axios";
+import SimpleComponent from "./SimpleComponent.vue";
 
 export default {
-  data() {
-    return {
-      posts: [],
-      errors: []
-    };
+  name: "app",
+  components: {
+    SimpleComponent
   },
 
-  // Fetches posts when the component is created.
-  created() {
-    var date = new Date();
-    date.setDate(date.getDate() + 1);
-    console.log();
-
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=` + date)
-      .then(response => {
-        // JSON responses are automatically parsed.
-        this.posts[0] = response.data;
-      })
-      .catch(e => {
-        this.errors.push(e);
-      });
-
-    // async / await version (created() becomes async created())
-    //
-    // try {
-    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    //   this.posts = response.data
-    // } catch (e) {
-    //   this.errors.push(e)
-    // }
+  data() {
+    return {
+      msg: "My Profile Component"
+    };
   }
 };
 </script>
