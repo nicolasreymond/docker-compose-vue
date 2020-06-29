4f20c6a96f0fa7f6bb724837743dd15f6b42053c
Nicolas Reymond <n.reymond.12@gmail.com>
2020-06-29 13:05:54 +0200

    [F.T] Add Delete function

--- api/produits.php
+++ api/produits.php
@@ -6,7 +6,6 @@
 	function getProducts()
 	{
 		
-		global $conn;
 		$query = "SELECT * FROM posts";
 		$response = array();
 		$stmt = EDatabase::prepare($query);
@@ -18,11 +17,10 @@
 	
 	function getProduct($id=0)
 	{
-		global $conn;
 		$query = "SELECT * FROM posts";
 		if($id > 0)
 		{
-			$query .= " WHERE IDPosts=".$id." LIMIT 1";
+			$query .= " WHERE IDPosts=:id LIMIT 1";
 		}
 		$response = array();
 		$stmt = EDatabase::prepare($query);
@@ -30,6 +28,46 @@
 		$response = $stmt->fetchAll(PDO::FETCH_BOTH);
 		header('Content-Type: application/json');
 		echo json_encode($response, JSON_PRETTY_PRINT);
+	}
+
+	function AddProduct()
+	{
+
+		// On retourne l'id du record inséré
+		$sql = " INSERT INTO posts (commentaire, datePost, postType) VALUES (:c, :ts, :pt)";
+		$sth = EDatabase::prepare($sql);
+		try {
+			$sth->execute(array(
+				':c' => $_POST["commentaire"],
+				':ts' => date("Y-m-d"),
+				':pt' => $_POST["postType"]
+			));
+		} catch (PDOException $e) {
+			echo 'Problème de lecture de la base de données: ' . $e->getMessage();
+			EDatabase::rollBack();
+			return false;
+		}
+		
+		header('Content-Type: application/json');
+		echo json_encode($sth);
+	}
+
+	function deleteProduct($id)
+	{
+		$query = "DELETE FROM posts WHERE IDPosts=:id";
+		$sth = EDatabase::prepare($query);
+		var_dump($id);
+		try {
+			$sth->bindParam(':id', $id, PDO::PARAM_INT);   
+			$sth->execute();
+		} catch (PDOException $e) {
+			echo 'Problème de lecture de la base de données: ' . $e->getMessage();
+			EDatabase::rollBack();
+			return false;
+		}
+
+		header('Content-Type: application/json');
+		echo json_encode($sth);
 	}
 	
 
@@ -48,6 +86,18 @@
 				getProducts();
 			}
 			break;
+
+		case 'POST':
+			// Ajouter un produit
+			AddProduct();
+			break;
+
+		case 'DELETE':
+    		// Supprimer un produit
+			$id = intval($_GET["id"]);
+			deleteProduct($id);
+			break;
+
 		default:
 			// Invalid Request Method
 			header("HTTP/1.0 405 Method Not Allowed");
